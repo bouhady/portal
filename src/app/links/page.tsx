db095ec53f62a7bd91a72187c52170368c639a32
Yaniv Bouhadana <yanivb@deliveright.com>
2024-06-04 23:13:45 +0300

    add all links

---
+++ src/app/links/page.tsx
@@ -0,0 +1,34 @@
+import styles from "./page.module.css";
+export default function Home() {
+  return (
+    <main className={ styles.main }>
+      <div className={ styles.header } aria-description="header">
+        הר לעמק 2024
+      </div>
+      <div className={ styles.links }>
+
+        קטע 1 : <a href={ 'https://connect.garmin.com/modern/course/275741798' }> https://connect.garmin.com/modern/course/275741798 </a>
+        קטע 2 : <a href={ 'https://connect.garmin.com/modern/course/275742049' }> https://connect.garmin.com/modern/course/275742049 </a>
+        קטע 3 : <a href={ 'https://connect.garmin.com/modern/course/275742194' }> https://connect.garmin.com/modern/course/275742194 </a>
+        קטע 4 : <a href={ 'https://connect.garmin.com/modern/course/275742394' }> https://connect.garmin.com/modern/course/275742394 </a>
+        קטע 5 : <a href={ 'https://connect.garmin.com/modern/course/276151900' }> https://connect.garmin.com/modern/course/276151900 </a>
+        קטע 6 : <a href={ 'https://connect.garmin.com/modern/course/276151997' }> https://connect.garmin.com/modern/course/276151997 </a>
+        קטע 7 : <a href={ 'https://connect.garmin.com/modern/course/276152096' }> https://connect.garmin.com/modern/course/276152096 </a>
+        קטע 8 : <a href={ 'https://connect.garmin.com/modern/course/276152198' }> https://connect.garmin.com/modern/course/276152198 </a>
+        קטע 9 : <a href={ 'https://connect.garmin.com/modern/course/276152272' }> https://connect.garmin.com/modern/course/276152272 </a>
+        קטע 10 : <a href={ 'https://connect.garmin.com/modern/course/276152354' }> https://connect.garmin.com/modern/course/276152354 </a>
+        קטע 11 : <a href={ 'https://connect.garmin.com/modern/course/276152430' }> https://connect.garmin.com/modern/course/276152430 </a>
+        קטע 12 : <a href={ 'https://connect.garmin.com/modern/course/276152542' }> https://connect.garmin.com/modern/course/276152542 </a>
+        קטע 13 : <a href={ 'https://connect.garmin.com/modern/course/276152614' }> https://connect.garmin.com/modern/course/276152614 </a>
+        קטע 14 : <a href={ 'https://connect.garmin.com/modern/course/276152724' }> https://connect.garmin.com/modern/course/276152724 </a>
+        קטע 15 : <a href={ 'https://connect.garmin.com/modern/course/276152773' }> https://connect.garmin.com/modern/course/276152773 </a>
+        קטע 16 : <a href={ 'https://connect.garmin.com/modern/course/276152865' }> https://connect.garmin.com/modern/course/276152865 </a>
+        קטע 17 : <a href={ 'https://connect.garmin.com/modern/course/276152948' }> https://connect.garmin.com/modern/course/276152948 </a>
+        קטע 18 : <a href={ 'https://connect.garmin.com/modern/course/276153037' }> https://connect.garmin.com/modern/course/276153037 </a>
+
+
+      </div>
+
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
+import styles from "./page.module.css";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="" aria-description="header">
-        ברוכים הבאים לאתר של יניב
+    <main className={ styles.main }>
+      <div className={ styles.header } aria-description="header">
+        הר לעמק 2024
       </div>
+      <div className={ styles.grid }>
+        <iframe src='https://connect.garmin.com/modern/course/embed/275741798' width='465' height='548'/>
+        <iframe src='https://connect.garmin.com/modern/course/embed/275742049' width='465' height='548'/>
+        <iframe src='https://connect.garmin.com/modern/course/embed/275742194' width='465' height='548'/>
+        <iframe src='https://connect.garmin.com/modern/course/embed/275742394' width='465' height='548'/>
+        <iframe src='https://connect.garmin.com/modern/course/embed/276151900' width='465' height='548'/>
+        <iframe src='https://connect.garmin.com/modern/course/embed/276151997' width='465' height='548'/>
+        <iframe src='https://connect.garmin.com/modern/course/embed/276152096' width='465' height='548'/>
+        <iframe src='https://connect.garmin.com/modern/course/embed/276152198' width='465' height='548'/>
+        <iframe src='https://connect.garmin.com/modern/course/embed/276152272' width='465' height='548'/>
+        <iframe src='https://connect.garmin.com/modern/course/embed/276152354' width='465' height='548'/>
+        <iframe src='https://connect.garmin.com/modern/course/embed/276152430' width='465' height='548'/>
+        <iframe src='https://connect.garmin.com/modern/course/embed/276152542' width='465' height='548'/>
+        <iframe src='https://connect.garmin.com/modern/course/embed/276152614' width='465' height='548'/>
+        <iframe src='https://connect.garmin.com/modern/course/embed/276152724' width='465' height='548'/>
+        <iframe src='https://connect.garmin.com/modern/course/embed/276152773' width='465' height='548'/>
+        <iframe src='https://connect.garmin.com/modern/course/embed/276152865' width='465' height='548'/>
+        <iframe src='https://connect.garmin.com/modern/course/embed/276152948' width='465' height='548'/>
+        <iframe src='https://connect.garmin.com/modern/course/embed/276153037' width='465' height='548'/>
+      </div>
+
     </main>
   );
 }
